e15e7aae03f75ba35104c8cba9f2abb1bfcd7dc3
shahriar-fattahi <alifattahisf@gmail.com>
2024-08-16 23:30:45 +0330

    feat(storage): add storage model

--- key_value/storage/models.py
+++ key_value/storage/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Storage(models.Model):
+    class Meta:
+        indexes = [
+            models.Index(models.F("data__key"), name="data__key_idx"),
+        ]
+
+    data = models.JSONField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
