a952eedbbd3ed600f896ee31484bbc1b3ba6c805
shahriar-fattahi <alifattahisf@gmail.com>
2024-08-17 02:29:08 +0330

    feat(users): add storage model  to admin panels

--- key_value/storage/admin.py
+++ key_value/storage/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Storage
+
+
+@admin.register(Storage)
+class StorageAdmin(admin.ModelAdmin):
+    readonly_fields = ["data", "created_at", "updated_at"]
+    list_display = ("key", "value", "created_at", "updated_at", "id")
+
+    def key(self, obj):
+        return obj.data["key"]
+
+    def value(self, obj):
+        return obj.data["value"]
