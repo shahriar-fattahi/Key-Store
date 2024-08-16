5f505d834218c77e7bf3304fd88b2548e9c58545
shahriar-fattahi <alifattahisf@gmail.com>
2024-08-17 01:45:24 +0330

    feat(storage): add key-value list api

--- key_value/storage/urls.py
+++ key_value/storage/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .apis import GetValueApi, SetKeyApi
+from .apis import GetKeyValueListApi, GetValueApi, SetKeyApi
 
 app_name = "storage"
 
 urlpatterns = [
     path("set", SetKeyApi.as_view(), name="set-key"),
+    path("get", GetKeyValueListApi.as_view(), name="get-value"),
     path("get/<str:key_name>", GetValueApi.as_view(), name="get-value"),
 ]
