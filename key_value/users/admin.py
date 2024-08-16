2ceb8712f39ab85a7baaf5c10780d5f5cc1e82ca
shahriar-fattahi <alifattahisf@gmail.com>
2024-08-16 19:37:59 +0330

    chore: initial the project

--- key_value/users/admin.py
+++ key_value/users/admin.py
@@ -2,17 +2,11 @@ from allauth.account.decorators import secure_admin_login
 from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
-from django.utils.translation import gettext_lazy as _
+
 
 from .forms import UserAdminChangeForm
 from .forms import UserAdminCreationForm
 from .models import User
-
-if settings.DJANGO_ADMIN_FORCE_ALLAUTH:
-    # Force the `admin` sign in process to go through the `django-allauth` workflow:
-    # https://docs.allauth.org/en/latest/common/admin.html#admin
-    admin.autodiscover()
-    admin.site.login = secure_admin_login(admin.site.login)  # type: ignore[method-assign]
 
 
 @admin.register(User)
@@ -21,9 +15,9 @@ class UserAdmin(auth_admin.UserAdmin):
     add_form = UserAdminCreationForm
     fieldsets = (
         (None, {"fields": ("username", "password")}),
-        (_("Personal info"), {"fields": ("name", "email")}),
+        ("Personal info", {"fields": ("first_name","last_name", "email")}),
         (
-            _("Permissions"),
+            "Permissions",
             {
                 "fields": (
                     "is_active",
@@ -34,7 +28,7 @@ class UserAdmin(auth_admin.UserAdmin):
                 ),
             },
         ),
-        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
     )
-    list_display = ["username", "name", "is_superuser"]
-    search_fields = ["name"]
+    list_display = ["username", "is_superuser"]
+    search_fields = ["username"]
--- key_value/users/apps.py
+++ key_value/users/apps.py
@@ -1,13 +1,7 @@
-import contextlib
-
 from django.apps import AppConfig
-from django.utils.translation import gettext_lazy as _
+
 
 
 class UsersConfig(AppConfig):
     name = "key_value.users"
-    verbose_name = _("Users")
-
-    def ready(self):
-        with contextlib.suppress(ImportError):
-            import key_value.users.signals  # noqa: F401
+    verbose_name = "Users"
