1812d600f74b9a7432ea4528c8aaf97243ed2bd7
shahriar-fattahi <alifattahisf@gmail.com>
2024-08-17 01:49:10 +0330

    feat: add cache

--- key_value/storage/apis.py
+++ key_value/storage/apis.py
@@ -1,4 +1,6 @@
 from django.shortcuts import get_object_or_404
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView
 
 from .models import Storage
@@ -9,6 +11,7 @@ class SetKeyApi(CreateAPIView):
     serializer_class = SetKeySerializer
 
 
+@method_decorator(cache_page(60 * 60 * 2), name="get")
 class GetKeyValueListApi(ListAPIView):
     serializer_class = GetKeyValueSerializer
 
@@ -16,6 +19,7 @@ class GetKeyValueListApi(ListAPIView):
         return Storage.objects.all()
 
 
+@method_decorator(cache_page(60 * 60 * 2), name="get")
 class GetValueApi(RetrieveAPIView):
     lookup_url_kwarg = "key_name"
     serializer_class = GetKeyValueSerializer
