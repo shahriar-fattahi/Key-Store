1afbf1590dc19c40cd14b44b1556bd26cf739294
shahriar-fattahi <alifattahisf@gmail.com>
2024-08-17 01:29:28 +0330

    feat(storage): add get value api

--- key_value/storage/apis.py
+++ key_value/storage/apis.py
@@ -1,7 +1,17 @@
-from rest_framework.generics import CreateAPIView
+from django.shortcuts import get_object_or_404
+from rest_framework.generics import CreateAPIView, RetrieveAPIView
 
-from .serializers import SetKeySerializer
+from .models import Storage
+from .serializers import GetKeyValueSerializer, SetKeySerializer
 
 
 class SetKeyApi(CreateAPIView):
     serializer_class = SetKeySerializer
+
+
+class GetValueApi(RetrieveAPIView):
+    lookup_url_kwarg = "key_name"
+    serializer_class = GetKeyValueSerializer
+
+    def get_object(self):
+        return get_object_or_404(Storage, data__key=self.kwargs[self.lookup_url_kwarg])
